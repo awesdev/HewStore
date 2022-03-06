0337374285c0749f1b3511b17dfaf88a9578a240
awes <you@example.com>
2022-03-06 21:50:58 +0300

    make it better

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
 $(document).ready(function () {
-    $('.lang__btn').click(function (e) {
-        e.preventDefault();
-        $('.lang__btn').removeClass('lang__btn_active');
-        if ($(this).hasClass('lang__btn_eng')) {
-            $('.lang__btn_eng').addClass('lang__btn_active');
-        } else {
-            $('.lang__btn_ru').addClass('lang__btn_active');
-        }
-    });
-
-    $('.header__link').click(function (e) {
-        e.preventDefault();
+    $('.header__link').click(function () {
         $('.header__link').removeClass('header__link_active');
         $(this).addClass('header__link_active');
+    });
+
+    $('.header__burger-link').click(function () {
+        $('.header__burger-link').removeClass('header__burger-link_active');
+        $(this).addClass('header__burger-link_active');
     });
 
     $('.header__burger').click(function () {
@@ -20,7 +14,7 @@ $(document).ready(function () {
         $('.header__lang').toggleClass('header__lang_active');
         if ($(this).hasClass('header__burger_active'))
             $('.header__burger-container').fadeIn(100);
-            else
+        else
             $('.header__burger-container').fadeOut(100);
     });
 });
