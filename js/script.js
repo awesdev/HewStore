a554d3a11a000e57a22acd10a2f073540eab2061
awes <you@example.com>
2022-03-06 13:15:42 +0300

    make it better

--- js/script.js
+++ js/script.js
@@ -14,4 +14,13 @@ $(document).ready(function () {
         $('.header__link').removeClass('header__link_active');
         $(this).addClass('header__link_active');
     });
+
+    $('.header__burger').click(function () {
+        $(this).toggleClass('header__burger_active');
+        $('.header__lang').toggleClass('header__lang_active');
+        if ($(this).hasClass('header__burger_active'))
+            $('.header__burger-container').fadeIn(100);
+            else
+            $('.header__burger-container').fadeOut(100);
+    });
 });
